bb44f7ab6b747c44875c570f8f2cd6f14019b960
wenyongjiang <329837810@qq.com>
2021-12-24 16:21:52 +0800

    save

--- src/views/decoration/Main.vue
+++ src/views/decoration/Main.vue
@@ -4,6 +4,10 @@
       <ComponentPanel @to-add-component="onAddComponent" />
     </div>
     <div :class="css.preview_container">
+      <div>
+        <Btn type="primary"
+          @click="onSave">保存</Btn>
+      </div>
       <div :class="css.preview_box">
         <template v-for="com in pageComponents"
           :key="com.uid">
@@ -18,7 +22,10 @@
     </div>
   </div>
 </template>
-<script lang="ts" setup>
+<script lang="tsx" setup>
+import Btn from 'will-ui/components/btn/index'
+import Notice from 'will-ui/components/notice/index'
+import Icon from 'will-ui/components/icon/index'
 import type { ComponentDescription } from 'decoration-types'
 import {
   ComponentDescriptionKey,
@@ -27,9 +34,10 @@ import {
   UpdateComponentKey
 } from 'decoration-symbols'
 import { createUid } from 'decoration-modules'
-import { ref, computed, provide, onMounted, readonly } from 'vue'
+import { ref, computed, provide, onMounted, readonly, useCssModule } from 'vue'
 import ComponentPanel from './components/ComponentPanel.vue'
 import ComponentOptions from './components/ComponentOptions.vue'
+const css = useCssModule('css')
 
 /**
  * 当前装修页面中的组件列表
@@ -74,8 +82,29 @@ provide(DeleteComponentKey, (uid) => {
 onMounted(() => {
   // console.log('Main组件挂载')
 })
+
+function onSave() {
+  if(pageComponents.value.length===0) {
+    Notice.open({
+      content:()=> {
+        return <div class={css.notice}>
+          <Icon name="w-icon-warning" />
+          <span>还没有选择任何组件！</span>
+        </div>
+      }
+    })
+  } else {
+    console.log(JSON.parse(JSON.stringify(pageComponents.value)))
+  }
+}
 </script>
 <style lang="postcss" module="css">
+.notice {
+  padding: 20px;
+  background-color: rgb(80, 80, 80);
+  color: white;
+  border-radius: var(--w-radius);
+}
 .decoration {
   display: flex;
   height: 100%;
@@ -91,6 +120,7 @@ onMounted(() => {
     display: flex;
     align-items: center;
     justify-content: center;
+    flex-direction: column;
     flex: 1;
     height: 100%;
     padding: var(--w-padding-x);
@@ -99,6 +129,7 @@ onMounted(() => {
       width: 375px;
       height: 667px;
       background-color: white;
+      margin-top: var(--w-gap);
       overflow: auto;
     }
   }
